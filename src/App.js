58589d4a62ef458f5417daf9f422c05bdd9981bd
CurrentJob <78770258+currentJob@users.noreply.github.com>
2026-04-24 16:11:46 +0900

    ✨ Feat: React to Github Pages
    React 기반 Github Pages 배포

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>GitHub Pages</h1>
+      <p>React + GitHub Actions</p>
+    </div>
+  );
+}
+
+export default App;
